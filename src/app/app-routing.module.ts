217ea7dfbe295e1d48ce2e05a1882886c171af37
Michal-Kozlowski <michalandrzejkozlowski@gmail.com>
2018-03-12 11:39:56 +0100

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LibraryComponent } from './library/library.component';
@@ -12,20 +11,17 @@ import { UserComponent } from './user/user.component';
 const routes: Routes = [
   { path: 'library', component: LibraryComponent },
   { path: 'library/:id', component: BookComponent },
-  { path: 'welcome', component: WelcomeComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'user', component: UserComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '', component: WelcomeComponent },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [
-    // CommonModule
     RouterModule.forRoot(routes)
   ],
-  // declarations: []
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
